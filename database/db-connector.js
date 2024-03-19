e8ba273893ac4ae1441d33b4701fd396d960cf90
Guibely <107956206+Guibely@users.noreply.github.com>
2024-03-19 12:21:59 -0500

    Create db-connector.js

---
+++ database/db-connector.js
@@ -0,0 +1,23 @@
+/* 
+Students: Guibely De Aguinaga and Ignacio De La Cruz
+Assignment: CS340 Project
+Course: CS340 - Group 39
+Code Citations: ALL code sections within this file uses code from
+https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%201%20-%20Connecting%20to%20a%20MySQL%20Database as the skeleton code.
+*/
+// ./database/db-connector.js
+
+// Get an instance of mysql we can use in the app
+var mysql = require('mysql')
+
+// Create a 'connection pool' using the provided credentials
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'classmysql.engr.oregonstate.edu',
+    user: 
+    password: 
+    database: 
+})
+
+// Export it for use in our applicaiton
+module.exports.pool = pool;
